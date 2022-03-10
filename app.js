445e137fa7fc794fcdd6a8b26da75c796bf93ac2
Nasir <nasir-islam@hotmail.com>
2022-03-10 11:43:06 +0000

    add basis for swap player functionality

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 console.log("JS is working")
 
+// Boolean to determine who's turn it is - start with player 1 - X
+let isCirclePlayer = false;
+
 const gridList = document.querySelectorAll(".grid");
 
 
 for(let i =0; i < gridList.length; i++){
-    addGridIcon(gridList[i])
+    addGridIconEventListener(gridList[i])
 }
 
-function addGridIcon (oneGrid){ // Named function to hoist to the top
+function addGridIconEventListener (oneGrid){ // Named function to hoist to the top
     oneGrid.addEventListener('click', () => {
         console.log("clicked on grid")
         let icon = document.createElement('i');
         icon.classList.add("fa-solid");
-        icon.classList.add("fa-x");
         icon.classList.add("fa-6x");
+
+        if(isCirclePlayer){
+            icon.classList.add("fa-o");
+        } else{
+            icon.classList.add("fa-x");
+        }
+        // Once icon is made - place in the grid
         oneGrid.appendChild(icon);
+        // Switch turns
+        isCirclePlayer = !isCirclePlayer;
     })
 }
