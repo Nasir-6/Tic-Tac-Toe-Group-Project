aeb35cbb321b34567f0307ccc79045ddb38f7e9b
Michael Atayero <michaelak1395@gmail.com>
2022-03-10 12:43:26 +0000

    back on track

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 console.log("JS is working")
-
+console.log("testing git")
 
 // Boolean to determine who's turn it is - start with player 1 - X
 let isCirclePlayer = false;
 
 const gridList = document.querySelectorAll(".grid");
-
+const WINNINGCOMBINAITONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+];
 
 for(let i =0; i < gridList.length; i++){
     addGridIconEventListener(gridList[i])
@@ -19,6 +28,7 @@ function addGridIconEventListener(oneGrid) {
     // Boolean to see if the grid is used (grab the class)
     isUsed = oneGrid.classList[2] === "used";
     console.log("Grid is being occupied by " + oneGrid.classList[1])
+    
 
 
     if (!isUsed) {
@@ -29,8 +39,8 @@ function addGridIconEventListener(oneGrid) {
 
       if (isCirclePlayer) {
         icon.classList.add("fa-o");
-        // keep track of what is inside of the grid (o being placed)
-        oneGrid.classList.add("o")
+         // keep track of what is inside of the grid (o being placed)
+         oneGrid.classList.add("o")
       } else {
         icon.classList.add("fa-x");
         oneGrid.classList.add("x")
@@ -47,3 +57,30 @@ function addGridIconEventListener(oneGrid) {
     }
   });
 }
+
+function grabCurrentPlayerPositions (){
+    const playerPositions = [];
+    let iconToLookFor;
+    if(isCirclePlayer){
+        iconToLookFor = isCirclePlayer
+    }
+
+
+    for (const oneGrid of gridList) {
+        
+    }
+        
+    }
+}
+
+function checkIfCurrentPlayerWin() {
+    // grab all the indexes for the current player from gridList
+
+
+    //loop through winning combination
+    // check if there is a winning combination from current player indexes
+    // use indexOF.
+    //If win console.log "Player ... wins"
+    //Else console.log ("No winning combination")
+
+} 
