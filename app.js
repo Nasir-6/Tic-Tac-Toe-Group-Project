48a42034df7d75cbda25e8fa3f24303952925d86
Michael Atayero <michaelak1395@gmail.com>
2022-03-10 12:52:49 +0000

    the machines

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("JS is working")
-console.log("testing git")
+console.log("something else")
 
 // Boolean to determine who's turn it is - start with player 1 - X
 let isCirclePlayer = false;
@@ -16,62 +16,60 @@ const WINNINGCOMBINAITONS = [
     [6, 4, 2]
 ];
 
-for(let i =0; i < gridList.length; i++){
+for (let i = 0; i < gridList.length; i++) {
     addGridIconEventListener(gridList[i])
 }
 
 function addGridIconEventListener(oneGrid) {
-  // Named function to hoist to the top
-  oneGrid.addEventListener("click", () => {
-    
-    console.log(oneGrid.classList)
-    // Boolean to see if the grid is used (grab the class)
-    isUsed = oneGrid.classList[2] === "used";
-    console.log("Grid is being occupied by " + oneGrid.classList[1])
-    
+    // Named function to hoist to the top
+    oneGrid.addEventListener("click", () => {
 
+        console.log(oneGrid.classList)
+        // Boolean to see if the grid is used (grab the class)
+        isUsed = oneGrid.classList[2] === "used";
+        console.log("Grid is being occupied by " + oneGrid.classList[1])
 
-    if (!isUsed) {
-      console.log("Setting Grid Icon");
-      let icon = document.createElement("i");
-      icon.classList.add("fa-solid");
-      icon.classList.add("fa-6x");
+        if (!isUsed) {
+            console.log("Setting Grid Icon");
+            let icon = document.createElement("i");
+            icon.classList.add("fa-solid");
+            icon.classList.add("fa-6x");
 
-      if (isCirclePlayer) {
-        icon.classList.add("fa-o");
-         // keep track of what is inside of the grid (o being placed)
-         oneGrid.classList.add("o")
-      } else {
-        icon.classList.add("fa-x");
-        oneGrid.classList.add("x")
-      }
-      // Once icon is made - place in the grid
-      oneGrid.appendChild(icon);
-      // Switch turns
-      isCirclePlayer = !isCirclePlayer;
+            if (isCirclePlayer) {
+                icon.classList.add("fa-o");
+                // keep track of what is inside of the grid (o being placed)
+                oneGrid.classList.add("o")
+            } else {
+                icon.classList.add("fa-x");
+                oneGrid.classList.add("x")
+            }
+            // Once icon is made - place in the grid
+            oneGrid.appendChild(icon);
+            // Switch turns
+            isCirclePlayer = !isCirclePlayer;
 
-      // Set grid to used (add used class)
-      oneGrid.classList.add("used")
-    } else{
-        console.log("This Grid is used")
-    }
-  });
+            // Set grid to used (add used class)
+            oneGrid.classList.add("used")
+        } else {
+            console.log("This Grid is used")
+        }
+    });
 }
 
-function grabCurrentPlayerPositions (){
+function grabCurrentPlayerPositions() {
     const playerPositions = [];
     let iconToLookFor;
-    if(isCirclePlayer){
+    if (isCirclePlayer) {
         iconToLookFor = isCirclePlayer
     }
 
 
     for (const oneGrid of gridList) {
-        
+
     }
-        
-    }
+
 }
+
 
 function checkIfCurrentPlayerWin() {
     // grab all the indexes for the current player from gridList
