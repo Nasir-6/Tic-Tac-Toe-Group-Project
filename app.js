999f63091f5cc5b13c177b0ea8b85a4ae958d400
Cristian Rosca <cristianrrosca@gmail.com>
2022-03-10 11:31:00 +0000

    add addGridIcon function

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("JS is working")
+
+const gridList = document.querySelectorAll(".grid");
+
+
+for(let i =0; i < gridList.length; i++){
+    addGridIcon(gridList[i])
+}
+
+function addGridIcon (oneGrid){ // Named function to hoist to the top
+    oneGrid.addEventListener('click', () => {
+        console.log("clicked on grid")
+        let icon = document.createElement('i');
+        icon.classList.add("fa-solid");
+        icon.classList.add("fa-x");
+        icon.classList.add("fa-6x");
+        oneGrid.appendChild(icon);
+    })
+}
