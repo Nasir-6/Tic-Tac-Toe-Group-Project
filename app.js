b6ad78b0c7350f9ed5ae9d22ba08ed1620b92d9b
Nasir <nasir-islam@hotmail.com>
2022-03-10 12:00:33 +0000

    add isUsed flag to prevent adding icon to occupied grid

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log("JS is working")
 
+
 // Boolean to determine who's turn it is - start with player 1 - X
 let isCirclePlayer = false;
 
@@ -10,21 +11,34 @@ for(let i =0; i < gridList.length; i++){
     addGridIconEventListener(gridList[i])
 }
 
-function addGridIconEventListener (oneGrid){ // Named function to hoist to the top
-    oneGrid.addEventListener('click', () => {
-        console.log("clicked on grid")
-        let icon = document.createElement('i');
-        icon.classList.add("fa-solid");
-        icon.classList.add("fa-6x");
+function addGridIconEventListener(oneGrid) {
+  // Named function to hoist to the top
+  oneGrid.addEventListener("click", () => {
+    
+    console.log(oneGrid.classList)
+    // Boolean to see if the grid is used (grab the class)
+    isUsed = oneGrid.classList[1] === "used";
 
-        if(isCirclePlayer){
-            icon.classList.add("fa-o");
-        } else{
-            icon.classList.add("fa-x");
-        }
-        // Once icon is made - place in the grid
-        oneGrid.appendChild(icon);
-        // Switch turns
-        isCirclePlayer = !isCirclePlayer;
-    })
+    if (!isUsed) {
+      console.log("Setting Grid Icon");
+      let icon = document.createElement("i");
+      icon.classList.add("fa-solid");
+      icon.classList.add("fa-6x");
+
+      if (isCirclePlayer) {
+        icon.classList.add("fa-o");
+      } else {
+        icon.classList.add("fa-x");
+      }
+      // Once icon is made - place in the grid
+      oneGrid.appendChild(icon);
+      // Switch turns
+      isCirclePlayer = !isCirclePlayer;
+
+      // Set grid to used (add used class)
+      oneGrid.classList.add("used")
+    } else{
+        console.log("This Grid is used")
+    }
+  });
 }
